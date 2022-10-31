d7636672d1dab491505f9dc5a6cc8177d4caf4f3
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-10-31 18:24:58 -0300

    New cards

--- js/flashcard.js
+++ js/flashcard.js
@@ -50,9 +50,7 @@
         function shuffleArray(array) {
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
-                const temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                [array[i], array[j]] = [array[j], array[i]];
             }
         }
 
