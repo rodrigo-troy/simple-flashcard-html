aab90ce1f9ee0d53e7352187f28278470e374788
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-07-13 14:50:42 -0400

    "Refactor flashcards.js to use async/await and enhance JSON data
    This commit updates the XMLHttpRequest in flashcards.js to use the more modern async/await syntax for performing the GET request to 'phrases.json'. This change simplifies the code, makes it easier to reason about, and reduces the chance of errors. It also uses the more efficient array.map function instead of array.forEach. In addition, the phrases.json file is updated with additional data to further help users in learning languages."

--- js/flashcard.js
+++ js/flashcard.js
@@ -1,87 +1,74 @@
 /*jshint esversion: 8 */
 /*jshint browser: true */
 /* global console*/
-(function () {
+(async () => {
     console.log("Initializing...");
 
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.overrideMimeType("application/json");
-    httpRequest.open('GET',
-                     'phrases.json',
-                     true);
+    const response = await fetch('phrases.json');
+    const json = await response.json();
 
-    httpRequest.onreadystatechange = function () {
-        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-            loadJSON(httpRequest.responseText);
-        }
-    };
+    function getDivFront(phrase) {
+        const divFront = document.createElement('div');
+        divFront.className = 'flip-card-front';
+        const divFrontP = document.createElement('p');
 
-    httpRequest.send(null);
-
-    function loadJSON(response) {
-        let json = JSON.parse(response);
-
-        function getDivFront(phrase) {
-            const divFront = document.createElement('div');
-            divFront.className = 'flip-card-front';
-            const divFrontP = document.createElement('p')
-
-            if (phrase.hasOwnProperty('spanish') && Array.isArray(phrase['spanish'])) {
-                divFrontP.innerText = phrase['spanish'][Math.floor(Math.random() * phrase['spanish'].length)];
-            }
-
-            divFront.appendChild(divFrontP);
-            return divFront;
+        if (phrase.hasOwnProperty('spanish') && Array.isArray(phrase['spanish'])) {
+            divFrontP.textContent = phrase['spanish'][Math.floor(Math.random() * phrase['spanish'].length)];
         }
 
-        function getDivBack(phrase) {
-            const divBack = document.createElement('div');
-            divBack.className = 'flip-card-back';
-
-            if (phrase.hasOwnProperty('english') && Array.isArray(phrase['english'])) {
-                phrase['english'].forEach(englishPhrase => {
-                    const divBackP = document.createElement('p');
-                    if (phrase.hasOwnProperty('bold') && Array.isArray(phrase['bold'])) {
-                        phrase['bold'].forEach((bold) => {
-                            if (englishPhrase.includes(bold)) {
-                                englishPhrase = englishPhrase.replace(bold,
-                                                                      '<b>' + bold + '</b>')
-                            }
-                        });
-                    }
-
-                    divBackP.innerHTML = englishPhrase;
-                    divBack.appendChild(divBackP);
-                });
-            }
-
-            return divBack;
-        }
-
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
-
-        shuffleArray(json['phrases']);
-
-        json['phrases'].forEach((phrase) => {
-            const div = document.createElement('div');
-            div.className = "flip-card";
-
-            const divChild = document.createElement('div');
-            divChild.className = 'flip-card-inner';
-
-            div.appendChild(divChild);
-
-            divChild.appendChild(getDivFront(phrase));
-            divChild.appendChild(getDivBack(phrase));
-
-            document.getElementById('root').appendChild(div);
-        });
+        divFront.appendChild(divFrontP);
+        return divFront;
     }
 
+    function getDivBack(phrase) {
+        const divBack = document.createElement('div');
+        divBack.className = 'flip-card-back';
+
+        if (phrase.hasOwnProperty('english') && Array.isArray(phrase['english'])) {
+            phrase['english'].map(englishPhrase => {
+                const divBackP = document.createElement('p');
+                if (phrase.hasOwnProperty('bold') && Array.isArray(phrase['bold'])) {
+                    phrase['bold'].map(bold => {
+                        if (englishPhrase.includes(bold)) {
+                            englishPhrase = englishPhrase.replace(bold, `<b>${bold}</b>`);
+                        }
+                    });
+                }
+
+                divBackP.innerHTML = englishPhrase;
+                divBack.appendChild(divBackP);
+            });
+        }
+
+        return divBack;
+    }
+
+    function shuffleArray(array) {
+        let m = array.length, t, i;
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+
+    json['phrases'] = shuffleArray(json['phrases']);
+
+    json['phrases'].forEach(phrase => {
+        const div = document.createElement('div');
+        div.className = "flip-card";
+
+        const divChild = document.createElement('div');
+        divChild.className = 'flip-card-inner';
+
+        div.appendChild(divChild);
+
+        divChild.appendChild(getDivFront(phrase));
+        divChild.appendChild(getDivBack(phrase));
+
+        document.getElementById('root').appendChild(div);
+    });
     console.log("Initialized...");
 })();
